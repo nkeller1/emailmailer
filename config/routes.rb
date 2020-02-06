e7783c08282085f4209d10fb5265e5a53c7027ab
Dione Wilson <dionew1@gmail.com>
2020-02-06 11:36:21 -0700

    Complete user can log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get '/registration', to: "registration#new"
-  post '/users', to: "registration#create"
-  get '/advice', to: "advice#show"
+  get   '/registration', to: "registration#new"
+  post  '/users', to: "registration#create"
+  get   '/advice', to: "advice#show"
+  get   '/login', to: "sessions#new"
+  post  '/login', to: "sessions#create"
 end
