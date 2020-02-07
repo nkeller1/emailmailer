662b47f872688f00850025d397040a0462479b66
Dione Wilson <dionew1@gmail.com>
2020-02-07 16:06:56 -0700

    Add spacing for readability in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   root to: 'welcome#show'
+
   get     '/registration', to: "registration#new"
+
   post    '/users', to: "registration#create"
+
   get     '/advice', to: "advice#show"
+  post    '/advice', to: "advice#create"
+
   get     '/login', to: "sessions#new"
   post    '/login', to: "sessions#create"
   delete  '/logout', to: "sessions#destroy"
