17eccf1be80befc968eb965acff842e2d2af03ec
Dione Wilson <dionew1@gmail.com>
2020-02-07 14:14:17 -0700

    Fix routes typo & add favicon

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get     '/advice', to: "advice#show"
   get     '/login', to: "sessions#new"
   post    '/login', to: "sessions#create"
-  delete  'logout', to: "sessions#destroy"
+  delete  '/logout', to: "sessions#destroy"
 end
